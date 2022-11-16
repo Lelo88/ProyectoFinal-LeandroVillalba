113ea8ff4583d3a3cb4e6f9c498e4139d22ba8d7
Leandro Gabriel Villalba <villalbaleandrogabriel@gmail.com>
2022-11-16 10:01:41 -0300

    funcion agregado de notas completada

--- ProyectoFinal/AppNotas/urls.py
+++ ProyectoFinal/AppNotas/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 
 from .views import inicio_nota, agregado_notas
 
-from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('notas/', inicio_nota, name = 'Notas'),
--- ProyectoFinal/AppNotas/views.py
+++ ProyectoFinal/AppNotas/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Nota
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
+from .forms import CrearNota
 # Create your views here.
 def inicio_nota(request):
     notas = Nota.objects.all()
     return render(request, 'notas_inicio.html',{'notas': notas})
 
 def agregado_notas(request):
-    return render(request, 'agregado_notas.html')
+    if request.method == 'POST':
+        notas = CrearNota(request.POST)
+        
+        if notas.is_valid():
+            nota = notas.save(commit = False)
+            nota.autor =request.user
+            nota.save()
+            
+            return redirect('Notas')
+    else:
+        notas = CrearNota()
+            
+    return render(request, 'agregado_notas.html',{'notas':notas})
